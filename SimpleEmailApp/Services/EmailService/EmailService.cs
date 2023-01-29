03b8b97b4c979bbf014428812e570e80b6f75246
Sihle Chonco <sihle.chonco@falcorp.co.za>
2023-01-29 18:16:44 +0200

    Add project files.

---
+++ SimpleEmailApp/Services/EmailService/EmailService.cs
@@ -0,0 +1,31 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit.Text;
+using MimeKit;
+
+namespace SimpleEmailApp.Services.EmailService
+{
+
+    public class EmailService : IEmailService
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        void IEmailService.SendEmail(EmailDto request)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(_configuration.GetSection("EmailUserName").Value));
+            email.To.Add(MailboxAddress.Parse(request.To));
+            email.Subject = request.Subject;
+            email.Body = new TextPart(TextFormat.Html) { Text = request.Body };
+
+            using var smtp = new SmtpClient();
+            smtp.Connect(_configuration.GetSection("EmailHost").Value, 587, MailKit.Security.SecureSocketOptions.StartTls);
+            smtp.Authenticate(_configuration.GetSection("EmailUserName").Value, _configuration.GetSection("EmailPassword").Value);
+            smtp.Send(email);
+            smtp.Disconnect(true);
+        }
+    }
+}
